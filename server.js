985f948c78f753e87d9b876256f172c9071d9260
Nick Eagan <neagan26@gmail.com>
2015-05-04 23:29:15 -0700

    add server with initial routes

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  if (req.url === '/time') {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+
+    var date = new Date();
+    res.write(date.getHours() + ':' + date.getMinutes());
+    res.end();
+  } else if (req.url === '/greet') {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+
+    if (req.method === 'POST') {
+      req.on('data', function(data) {
+        var body = JSON.parse(data.toString('utf-8'));
+        res.write(JSON.stringify({msg: 'hello ' + body.name}));
+        res.end();
+      });
+    } else {
+      res.write(JSON.stringify({msg: 'hello ' + req.url}));
+    }
+
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'application/json'
+    });
+
+    res.write(JSON.stringify({msg: 'page not found'}));
+    res.end();
+  }
+});
+
+server.listen(3000, function() {
+  console.log('Server started');
+});
